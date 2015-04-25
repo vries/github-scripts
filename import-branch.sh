96ae84cc712c0643bb1f5eaa176559344ef3fbfd
Tom de Vries <tom@codesourcery.com>
2015-04-25 11:42:49 +0200

    Add import-branch.sh

---
+++ import-branch.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+repo="$1"
+base="$2"
+branch="$3"
+torepo="$4"
+tobranch="$5"
+
+pwd=$(pwd -P)
+
+bundle="$pwd/$branch.bundle"
+rm -f "$bundle"
+
+(
+    cd "$repo"
+
+    git bundle create "$bundle" "$base..$branch"
+)
+
+(
+    cd "$torepo"
+
+    commit=$(git bundle list-heads "$bundle" "refs/heads/$branch" \
+	| awk '{print $1}')
+
+    git bundle verify "$bundle"
+
+    git bundle unbundle "$bundle"
+
+    git branch -D "$tobranch" \
+	|| true
+
+    git branch "$tobranch" "$commit"
+)
+
+rm "$bundle"
