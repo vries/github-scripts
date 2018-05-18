cd1a3836104beb47d7585419b2b357dc3e8a2d24
Tom de Vries <tom@codesourcery.com>
2018-05-18 13:18:50 +0200

    update-master: Add more branches

--- update-master.sh
+++ update-master.sh
@@ -21,3 +21,23 @@ git checkout gomp-4_0-branch
 git reset --hard svn/gomp-4_0-branch
 git push origin gomp-4_0-branch
 
+git checkout gcc-4_9-branch
+git reset --hard gcc-gnu-org/gcc-4_9-branch
+git push origin gcc-4_9-branch
+
+git checkout gcc-5-branch
+git reset --hard gcc-gnu-org/gcc-5-branch
+git push origin gcc-5-branch
+
+git checkout gcc-6-branch
+git reset --hard gcc-gnu-org/gcc-6-branch
+git push origin gcc-6-branch
+
+#git checkout gcc-7-branch
+#git reset --hard gcc-gnu-org/gcc-7-branch
+#git push origin gcc-7-branch
+
+#git checkout openacc-gcc-7-branch
+#git reset --hard gcc-gnu-org/openacc-gcc-7-branch
+#git push origin openacc-gcc-7-branch
+
